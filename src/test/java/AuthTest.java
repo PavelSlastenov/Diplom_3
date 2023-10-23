c972c456bc2484a107e31dd955544d44530bd916
“Slastenov <vtb4055018@vtb.ru>
2023-10-23 09:01:08 +0300

    added pom.xml

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,82 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import pages.MainPage;
+import pages.RegisterPage;
+
+@DisplayName("Авторизация")
+public class AuthTest extends BaseTest {
+    @Test
+    @Feature("Auth")
+    @Story("Auth")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Авторизация пользователя - успешная")
+    public void authTest() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickPersonalAreaButton();
+        mainPage.setEmailInput("andrey@yandex.ru");
+        mainPage.setPassInput("password");
+        mainPage.clickLoginButton();
+        mainPage.clickPersonalAreaButton();
+        mainPage.waitVisibleProfileText();
+        mainPage.shouldProfileText();
+    }
+
+    @Test
+    @Feature("Auth")
+    @Story("Auth")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Авторизация пользователя - кнопка на главном экране")
+    public void authMainButtonTest() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickLoginOrderButton();
+        mainPage.setEmailInput("andrey@yandex.ru");
+        mainPage.setPassInput("password");
+        mainPage.clickLoginButton();
+        mainPage.clickPersonalAreaButton();
+        mainPage.waitVisibleProfileText();
+        mainPage.shouldProfileText();
+    }
+
+    @Test
+    @Feature("Auth")
+    @Story("Auth")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Авторизация пользователя - форма регистрации")
+    public void authRegistrationFormButtonTest() {
+        MainPage mainPage = new MainPage(driver);
+        RegisterPage registerPage = new RegisterPage(driver);
+        mainPage.clickLoginOrderButton();
+        registerPage.clickRegisterButton();
+        mainPage.clickLoginRegistrationButton();
+        mainPage.setEmailInput("andrey@yandex.ru");
+        mainPage.setPassInput("password");
+        mainPage.clickLoginButton();
+        mainPage.clickPersonalAreaButton();
+        mainPage.waitVisibleProfileText();
+        mainPage.shouldProfileText();
+    }
+
+    @Test
+    @Feature("Auth")
+    @Story("Auth")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Авторизация пользователя - форма восстановления пароля")
+    public void authForgotFormButtonTest() {
+        MainPage mainPage = new MainPage(driver);
+        mainPage.clickLoginOrderButton();
+        mainPage.clickForgotPassButton();
+        mainPage.clickLoginForgotButton();
+        mainPage.setEmailInput("andrey@yandex.ru");
+        mainPage.setPassInput("password");
+        mainPage.clickLoginButton();
+        mainPage.clickPersonalAreaButton();
+        mainPage.waitVisibleProfileText();
+        mainPage.shouldProfileText();
+    }
+
+
+}
