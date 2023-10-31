3473864f79b495a27259d1ed985fdf1658286d13
“Slastenov <vtb4055018@vtb.ru>
2023-10-31 16:27:32 +0300

    refactoring

--- src/main/java/models/LoginUser.java
+++ src/main/java/models/LoginUser.java
@@ -1,10 +1,25 @@
 package models;
 
-import lombok.Data;
-
-@Data
 public class LoginUser {
+    public LoginUser(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
     private final String email;
     private final String password;
     private final String name;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
--- src/main/java/models/RegisterUser.java
+++ src/main/java/models/RegisterUser.java
@@ -1,10 +1,31 @@
 package models;
 
-import lombok.Data;
-
-@Data
 public class RegisterUser {
     private final String email;
     private final String password;
     private final String name;
+
+    public RegisterUser(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+
+
+
 }
